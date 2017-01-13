31f628573a083b62dcda5bf478e2a6e30172939a
Luis Felipe Jimenez Ferreira <xluis190@hotmail.com>
2017-01-13 00:20:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var quote;
 function generateQuote(){
    $.ajax({
-     url:"http://api.forismatic.com/api/1.0/?",
+     url:"https://api.forismatic.com/api/1.0/?",
      dataType:"jsonp",
     data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
     success: function(data){
